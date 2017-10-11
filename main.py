7d1c266971e7125c5891ee3f2572d5b3616fef6b
Tom Casavant <tominthehat@gmail.com>
2017-10-11 17:40:30 -0400

    Fixed generator issue, prints headlines

--- main.py
+++ main.py
@@ -9,15 +9,17 @@ import unidecode
 
 app = Flask(__name__)
 ask = Ask(app, "/")
-
+articles = 0
 @ask.launch
 def new_ask():
 	welcome = render_template('welcome')
 	
 	return question(welcome)
+
 @ask.on_session_started
 def new_session():
-    session['articles'] = article_generator()
+    global articles
+    articles = article_generator()
     
 def article_generator():
     feed = feedparser.parse('http://rss.nytimes.com/services/xml/rss/nyt/US.xml')
@@ -26,11 +28,13 @@ def article_generator():
         
 @ask.intent("GetArticlesIntent")
 def get_articles():
-    articles = session['articles']
+    global articles
     msg = next(articles).title
-    session['articles'] = articles
     return question(msg)
 
+@ask.intent('SummarizeIntent')
+def summarize();
+        
 
 @ask.intent("AMAZON.HelpIntent")
 def helpme():
